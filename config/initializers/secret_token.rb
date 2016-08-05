45399ef7eafc4d0efe9a784a078fb22c2e050ee8
IlianaP <ip97@cornell.edu>
2016-08-05 16:59:00 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TeamQ::Application.config.secret_key_base = '3c999712aa74389660c53630d7c41152409afd5b525561c436a5a506454e6dde81f23c60f344eeb4e97b0fee006a647282099e6a998e7fbe788dcd869c1d5a78'
